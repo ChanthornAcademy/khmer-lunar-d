49af62e6712584035e25b92dec45fcd41fd201b0
chanthornAcademy <hoeurnchanthorn@gmail.com>
2023-08-21 10:21:09 +0700

    fixed types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { lunar } from "khmercal";
 import { utils } from "./utils";
 import { KhmercalType } from "../types/lunar";
-import plugin, { formatRulesType } from "../types";
+import plugin from "../types";
 const { formatKhmerDate } = utils;
 import { constant } from "./constsant";
 
@@ -9,7 +9,7 @@ import { PluginFunc } from "dayjs";
 
 const toKhDate: PluginFunc<plugin.toKhDate> = (o, c, d) => {
   const proto = c.prototype;
-  proto.toKhDate = function (format?: formatRulesType) {
+  proto.toKhDate = function (format?: string) {
     const date = constant.kh.preparse(this.format());
     return formatKhmerDate(lunar(date) as KhmercalType, this, format);
   };
--- types/index.d.ts
+++ types/index.d.ts
@@ -3,27 +3,9 @@ import { PluginFunc } from "dayjs";
 
 declare const plugin: PluginFunc;
 export = plugin;
-
-export interface formatRulesType {
-  W: string;
-  w: string;
-  d: string;
-  D: string;
-  N: string;
-  n: string;
-  o: string;
-  m: string;
-  M: string;
-  a: string;
-  e: string;
-  b: string;
-  c: string;
-  j: string;
-}
-
 declare namespace plugin {
   export interface toKhDate {
-    toKhDate(format?: formatRulesType): string;
+    toKhDate(format?: string): string;
   }
 }
 
